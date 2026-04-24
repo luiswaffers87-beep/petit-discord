c453f4d14e570bc2d6db700cf807172f58e736c4
Luis Angel Garcia Muñoz <luiswaffers87@gmail.com>
2026-04-24 10:30:34 +0000

    Phase 3 . Pseudos uniques

--- mini_discord/lib/client_handler.ex
+++ mini_discord/lib/client_handler.ex
@@ -3,9 +3,7 @@ defmodule MiniDiscord.ClientHandler do
 
   def start(socket) do
     :gen_tcp.send(socket, "Bienvenue sur MiniDiscord!\r\n")
-    :gen_tcp.send(socket, "Entre ton pseudo : ")
-    {:ok, pseudo} = :gen_tcp.recv(socket, 0)
-    pseudo = String.trim(pseudo)
+    pseudo = choisir_pseudo(socket)
 
     :gen_tcp.send(socket, "Salons disponibles : #{salons_dispo()}\r\n")
     :gen_tcp.send(socket, "Rejoins un salon (ex: general) : ")
@@ -57,6 +55,7 @@ defmodule MiniDiscord.ClientHandler do
         Logger.info("Client déconnecté : #{inspect(reason)}")
         MiniDiscord.Salon.broadcast(salon, "👋 #{pseudo} a quitté ##{salon}\r\n")
         MiniDiscord.Salon.quitter(salon, self())
+        liberer_pseudo(pseudo)
     end
   end
 
@@ -66,4 +65,38 @@ defmodule MiniDiscord.ClientHandler do
       salons -> Enum.join(salons, ", ")
     end
   end
+
+  defp choisir_pseudo(socket) do
+    :gen_tcp.send(socket, "Entre ton pseudo : ")
+    {:ok, pseudo} = :gen_tcp.recv(socket, 0)
+    pseudo = String.trim(pseudo)
+# TODO : Si pseudo_disponible?(pseudo) -> reserver_pseudo(pseudo) et retourner pseudo
+    if pseudo_disponible?(pseudo) do
+      reserver_pseudo(pseudo)
+      pseudo
+# TODO : Sinon -> envoyer un message d'erreur et rappeler choisir_pseudo(socket)
+    else
+      :gen_tcp.send(socket, "Ce pseudo est déjà pris, choisis-en un autre.\r\n")
+      choisir_pseudo(socket)
+    end
+  end
+
+  defp pseudo_disponible?(pseudo) do
+# TODO : Vérifier avec :ets.lookup(:pseudos, pseudo) 
+# :ets.lookup retorna una lista. Si está vacía [], el pseudo es disponible
+# Retornar true si disponible, false sinon
+    :ets.lookup(:pseudos, pseudo) == []
+  end
+
+  defp reserver_pseudo(pseudo) do
+# TODO : Insérer dans :ets avec :ets.insert(:pseudos, {pseudo, self()})
+# self() es el PID del cliente actual
+  :ets.insert(:pseudos, {pseudo, self()})
+  end
+
+  defp liberer_pseudo(pseudo) do
+# TODO : Supprimer de :ets avec :ets.delete(:pseudos, pseudo)
+  :ets.delete(:pseudos, pseudo)
+  end
+
 end
